5c5f5501bd01804888462ed29c544f5b7cf49e22
irwanbekasi1993 <irwanbekasi1993@gmail.com>
2024-06-07 07:45:58 +0700

    .move db to nestjsprisma

--- prisma/migrations/20240607004424_init/migration.sql
+++ prisma/migrations/20240607004424_init/migration.sql
@@ -1,5 +1,16 @@
--- AlterTable
-ALTER TABLE "Article" ADD COLUMN     "authorId" INTEGER;
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "body" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" INTEGER,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "User" (
@@ -12,6 +23,9 @@ CREATE TABLE "User" (
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Article_title_key" ON "Article"("title");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
